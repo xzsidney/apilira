78af19fb13011827664ee90bfda2eeccd5d1e8e1
Sidney Lira <xzsidneyl@gmail.com>
2026-07-07 13:58:20 -0300

    revert: Remove hallucinated mysql driver adapter and return to standard Prisma Client

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaMysql } from "@prisma/adapter-mysql2";
-import mysql from "mysql2/promise";
 
-const connectionString = process.env.DATABASE_URL as string;
-const pool = mysql.createPool(connectionString);
-const adapter = new PrismaMysql(pool);
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 export default prisma;
